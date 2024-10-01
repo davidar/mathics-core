a2d43c9ebe01608142fe5342424f09f5270699d8
David A Roberts <d@vidr.cc>
2024-10-01 21:50:00 +1000

    Make formatting more generic

--- mathics/frontend/format.py
+++ mathics/frontend/format.py
@@ -26,109 +26,102 @@ FORM_TO_FORMAT = {
 }
 
 
-def format_output(obj, expr, format=None):
-    """
-    Handle unformatted output using the *specific* capabilities of mathics-django.
+class Formatter:
+    def format_output(self, evaluation, expr, format="unformatted"):
+        """
+        evaluation.py format_output() from which this was derived is similar but
+        it can't make use of a front-ends specific capabilities.
+        """
 
-    evaluation.py format_output() from which this was derived is similar but
-    it can't make use of a front-ends specific capabilities.
-    """
+        def eval_boxes(result, fn: Callable, evaluation, **options):
+            options["evaluation"] = evaluation
+            try:
+                boxes = fn(**options)
+            except BoxError:
+                boxes = None
+                if not hasattr(evaluation, "seen_box_error"):
+                    evaluation.seen_box_error = True
+                    evaluation.message(
+                        "General",
+                        "notboxes",
+                        Expression(SymbolFullForm, result).evaluate(evaluation),
+                    )
+            return boxes
 
-    def eval_boxes(result, fn: Callable, obj, **options):
-        options["evaluation"] = obj
-        try:
-            boxes = fn(**options)
-        except BoxError:
-            boxes = None
-            if not hasattr(obj, "seen_box_error"):
-                obj.seen_box_error = True
-                obj.message(
-                    "General",
-                    "notboxes",
-                    Expression(SymbolFullForm, result).evaluate(obj),
-                )
+        if isinstance(format, dict):
+            return dict(
+                (k, self.format_output(evaluation, expr, f)) for k, f in format.items()
+            )
 
-        return boxes
+        # For some expressions, we want formatting to be different.
+        # In particular for FullForm output, we don't want MathML, we want
+        # plain-ol' text so we can cut and paste that.
 
-    if format is None:
-        format = obj.format
+        expr_type = expr.get_head_name()
+        expr_head = expr.get_head()
+        if expr_head in (SymbolMathMLForm, SymbolTeXForm):
+            # For these forms, we strip off the outer "Form" part
+            format = FORM_TO_FORMAT[expr_type]
+            elements = expr.get_elements()
+            if len(elements) == 1:
+                expr = elements[0]
 
-    if isinstance(format, dict):
-        return dict((k, obj.format_output(expr, f)) for k, f in format.items())
+        if expr_head in (SymbolFullForm, SymbolOutputForm):
+            result = expr.elements[0].format(evaluation, expr_type)
+            return self.text(result.boxes_to_text())
+        # elif expr_head is SymbolGraphics:
+        #     result = Expression(SymbolStandardForm, expr).format(
+        #         evaluation, SymbolMathMLForm
+        #     )
 
-    # For some expressions, we want formatting to be different.
-    # In particular for FullForm output, we don't want MathML, we want
-    # plain-ol' text so we can cut and paste that.
+        # This part was derived from and the same as evaluation.py format_output.
 
-    expr_type = expr.get_head_name()
-    expr_head = expr.get_head()
-    if expr_head in (SymbolMathMLForm, SymbolTeXForm):
-        # For these forms, we strip off the outer "Form" part
-        format = FORM_TO_FORMAT[expr_type]
-        elements = expr.get_elements()
-        if len(elements) == 1:
-            expr = elements[0]
-
-    if expr_head in (SymbolFullForm, SymbolOutputForm):
-        result = expr.elements[0].format(obj, expr_type)
-        return result.boxes_to_text()
-    elif expr_head is SymbolGraphics:
-        result = Expression(SymbolStandardForm, expr).format(
-            obj, SymbolMathMLForm
+        use_quotes = get_settings_value(
+            evaluation.definitions, "Settings`$QuotedStrings"
         )
 
-    # This part was derived from and the same as evaluation.py format_output.
+        if format == "text":
+            result = expr.format(evaluation, SymbolOutputForm)
+            result = eval_boxes(result, result.boxes_to_text, evaluation)
 
-    use_quotes = get_settings_value(obj.definitions, "Settings`$QuotedStrings")
+            if use_quotes:
+                result = '"' + result + '"'
 
-    if format == "text":
-        result = expr.format(obj, SymbolOutputForm)
-        result = eval_boxes(result, result.boxes_to_text, obj)
-
-        if use_quotes:
-            result = '"' + result + '"'
-
-        return result
-    elif format == "xml":
-        result = Expression(SymbolStandardForm, expr).format(
-            obj, SymbolMathMLForm
-        )
-    elif format == "tex":
-        result = Expression(SymbolStandardForm, expr).format(
-            obj, SymbolTeXForm
-        )
-    elif format == "unformatted":
-        if expr_head is Symbol("Pymathics`Graph") and hasattr(expr, "G"):
-            return format_graph(expr.G)
-        if expr_head is SymbolCompiledFunction:
-            result = expr.format(obj, SymbolOutputForm)
+            return self.text(result)
+        elif format == "xml":
+            result = Expression(SymbolStandardForm, expr).format(
+                evaluation, SymbolMathMLForm
+            )
+            return self.html(eval_boxes(result, result.boxes_to_text, evaluation))
+        elif format == "tex":
+            result = Expression(SymbolStandardForm, expr).format(
+                evaluation, SymbolTeXForm
+            )
+            return self.math(eval_boxes(result, result.boxes_to_text, evaluation))
+        elif expr_head is Symbol("Pymathics`Graph") and hasattr(expr, "G"):
+            return self.svg(format_graph(expr.G))
+        elif expr_head is SymbolCompiledFunction:
+            result = expr.format(evaluation, SymbolOutputForm)
+            return self.text(eval_boxes(result, result.boxes_to_text, evaluation))
         elif expr_head is SymbolString:
-            result = expr.format(obj, SymbolInputForm)
+            result = expr.format(evaluation, SymbolInputForm)
             result = result.boxes_to_text()
 
             if not use_quotes:
                 # Substring without the quotes
                 result = result[1:-1]
 
-            return result
+            return self.text(result)
         elif expr_head is SymbolGraphics3D:
             form_expr = Expression(SymbolStandardForm, expr)
-            result = form_expr.format(obj, SymbolStandardForm)
-            return eval_boxes(result, result.boxes_to_json, obj)
+            result = form_expr.format(evaluation, SymbolStandardForm)
+            return self.graphics3d(eval_boxes(result, result.boxes_to_json, evaluation))
         elif expr_head is SymbolGraphics:
             form_expr = Expression(SymbolStandardForm, expr)
-            result = form_expr.format(obj, SymbolStandardForm)
-            return eval_boxes(result, result.boxes_to_svg, obj)
+            result = form_expr.format(evaluation, SymbolStandardForm)
+            return self.svg(eval_boxes(result, result.boxes_to_svg, evaluation))
         else:
             result = Expression(SymbolStandardForm, expr).format(
-                obj, SymbolTeXForm
+                evaluation, SymbolTeXForm
             )
-            return (
-                r"\[" + eval_boxes(result, result.boxes_to_text, obj) + r"\]"
-            )
-    else:
-        raise ValueError
-
-    if result is None:
-        return f"Error in evaluating {expr}"
-    return eval_boxes(result, result.boxes_to_text, obj)
+            return self.math(eval_boxes(result, result.boxes_to_text, evaluation))
--- mathics/frontend/jupyter.py
+++ mathics/frontend/jupyter.py
@@ -1,11 +1,32 @@
 import json
 
-from IPython.display import HTML, Math
+from IPython.display import Code, HTML, JSON, Math
 from IPython.core.magic import Magics, magics_class, line_cell_magic
 
 from mathics.session import MathicsSession
 
-from .format import format_output
+from .format import Formatter
+
+
+class JupyterFormatter(Formatter):
+    def text(self, result):
+        return Code(result, language="mathematica")
+
+    def math(self, result):
+        return Math(result)
+
+    def graphics3d(self, result):
+        return JSON(json.loads(result))
+
+    def svg(self, result):
+        return self.html(result)
+
+    def html(self, result):
+        result = result.replace("<math", "<div")
+        result = result.replace("<mglyph", '<img style="display: inline-block" ')
+        result = result.replace("<mrow>", "")
+        result = result.replace("<mo>", "")
+        return HTML(result)
 
 
 @magics_class
@@ -13,34 +34,12 @@ class MathicsMagic(Magics):
     def __init__(self, shell):
         super().__init__(shell)
         self.session = MathicsSession()
-        self.reset_session()
-
-    def reset_session(self):
-        self.session.reset()
-        self.session.evaluation.format = "unformatted"
-        self.session.evaluation.format_output = lambda expr, format: format_output(
-            self.session.evaluation, expr, format
-        )
+        self.formatter = JupyterFormatter()
 
     @line_cell_magic
     def mathics3(self, line, cell=""):
-        data = self.session.evaluate_as_in_cli(line + "\n" + cell).get_data()
-        result = data["result"].strip()
-        if result.startswith("<svg") or result.startswith("<math"):
-            result = result.replace("<math", "<div")
-            result = result.replace("<mglyph", '<img style="display: inline-block" ')
-            result = result.replace("<mrow>", "")
-            result = result.replace("<mo>", "")
-            return HTML(result)
-        if data["form"] == "TeXForm":
-            return Math(result)
-        if result.startswith(r"\["):
-            result = result.replace(r"\[", "")
-            result = result.replace(r"\]", "")
-            return Math(result)
-        if result.startswith('{"elements":'):
-            return json.loads(result)
-        return result
+        expr = self.session.evaluate(line + "\n" + cell)
+        return self.formatter.format_output(self.session.evaluation, expr)
 
 
 def load_ipython_extension(ipython):
